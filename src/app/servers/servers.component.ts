ce3538a5a8c5a6671dae8b32a530c07f8afa6c8e
storres8 <stevetorres7562@gmail.com>
2019-07-05 22:53:35 -0400

    commented out example on how to add relative paths through ActivatedRoute inside a component.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ServersService } from "./servers.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-servers",
@@ -9,9 +10,23 @@ import { ServersService } from "./servers.service";
 export class ServersComponent implements OnInit {
   private servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) {}
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    // ActivatedRoute hold the absolute path to this component.
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // .navigate() method does not know what current route we are on. So we can pass to it a second argument
+    // which is an object that tells is the current absolute path to get to this component. We can then use
+    // the first argument, the array of relative paths, to add to the absolute path to initiate a new url.
+    // With this.route from our constructor angular now knows the exact url path to this component.
+    //
+    // this.router.navigate(["servers"], { relativeTo: this.route });
   }
 }
