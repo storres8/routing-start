a70826726b6cf42c3d325c19101c8d9df239d420
storres8 <stevetorres7562@gmail.com>
2019-07-05 22:38:57 -0400

    update.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  // Inject Router to allows router access to the button onClick event onLoadServers.
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLoadServers() {
+    // .navigate take as an argument an array of the different segments of your path.
+    // Your path can be either absolute or relative.
+    this.router.navigate(["/servers"]);
   }
-
 }
